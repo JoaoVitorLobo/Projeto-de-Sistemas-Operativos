07e127c7f54f6827b9d8a6b63fd003d715fac983
joao vitor lobo <jovitorlobo@gmail.com>
2025-11-21 20:00:07 +0000

    ahhhhhh

--- SO-2526-Proj-codigo-base/SO-2526-Proj-codigo-base/src/game.c
+++ SO-2526-Proj-codigo-base/SO-2526-Proj-codigo-base/src/game.c
@@ -79,15 +79,43 @@ int read_line(int file,char* buffer){
     return i;
 }
 
+bool special_directory(char *d_name){
+    return  strcmp(d_name, ".") == 0 || strcmp(d_name, "..") == 0 ;
+}
+
+void build_filepath(char* file_path, char* dir_path, char* d_name){
+    strcpy(file_path, dir_path);
+    strcat(file_path, "/");
+    strcat(file_path,d_name);
+}
+
 int main(int argc, char** argv) {
+    char buffer[4096];
+    char** levels;
+    int accumulated_points = 0;
+    bool end_game = false;
+
+    board_t game_board;
+    board_t new_board;
+
+    DIR *dir;
+    struct dirent *entry;
+
+    int n_levels, n_monsters, n_pacmans = 0;
+
+    char* extension;
+
+    int file;
+    char file_path[512];
+
+    int ghost = 0;
+
+    pacman_t* pacman;
+
     if (argc != 2) {
         printf("Usage: %s <level_directory>\n", argv[0]);
         // TODO receive inputs
     }
-    char buffer[4096];
-    char** levels;
-
-    levels = malloc(sizeof(board_t));
 
     // Random seed for any random movements
     srand((unsigned int)time(NULL));
@@ -95,70 +123,77 @@ int main(int argc, char** argv) {
     open_debug_file("debug.log");
 
     terminal_init();
-    
-    int accumulated_points = 0;
-    bool end_game = false;
-    board_t game_board;
 
-    struct dirent *entry;
-    DIR *dir;
+    new_board = malloc(sizeof(board_t));
 
-    dir = opendir(argv[0]);
+    dir = opendir(argv[1]);
+
     if (dir == NULL) {
         perror("opendir");
         return -1;
     }
 
     while ((entry = readdir(dir)) != NULL) {
+        if (entry < 0) {
+            perror("readdir");
+            closedir(dir);
+            return -1;
+        }
         // ler o nome dos ficheiros
-        if  ( strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0 ) {
+        if  (special_directory(entry->d_name)) {
             continue; // skip . and ..
         }
-        char *name ;
-        int n_levels, n_monsters, n_pacmans = 0;
-        char* extension;
-        board_t* temp_board = malloc(sizeof(board_t));
+        new_board = malloc(sizeof(board_t));
         extension = strrchr(entry->d_name, '.'); //obtem a extensao do ficheiro
-        if(strcmp(extension,".lvl") == 0){
+
+        if (extension == NULL){
+            continue;
+        }
+
+        else if(strcmp(extension,".lvl") == 0){
             n_levels++;
-            char file_path[512];
-            strcpy(file_path, argv[0]);
-            strcat(file_path, "/");
-            strcat(file_path,entry->d_name);
-            int file = open(file_path, "O_RDONLY");
+
+            build_filepath(&file_path, argv[1], entry->d_name);
+
+            file = open(file_path, "O_RDONLY");
+
             if(file < 0){
                 perror("open");
+                closedir(dir);
                 return -1;
             }
-            char c;
+
             while (read_line(file, buffer) != 0){
-                int i;
+
                 if (strncmp(buffer, "DIM", 3) == 0){
-                    sscanf(buffer, "DIM %d %d\n", &temp_board->width, &temp_board->height);
+                    sscanf(buffer, "DIM %d %d\n", &new_board->width, &new_board->height);
                 }
-                else if (strncmp(buffer, "TEMPO\n", 5) == 0){
-                    sscanf(buffer, "TEMPO %d%n\n", &temp_board->tempo,&i);
+                else if (strncmp(buffer, "TEMPO", 5) == 0){
+                    sscanf(buffer, "TEMPO %d\n", &new_board->tempo);
                 }
                 else if (strncmp(buffer, "PAC", 3) == 0){
                     char pacman[256];
-                    i = 3;
-                    int ghost = 0;
-                    while(buffer[i] != '\n'){
-                        sscanf(buffer + i, " %s.p\n %n", pacman,&i);
-                        n_pacmans++;
-                        strcpy(file_path, argv[0]);
-                        strcat(file_path, "/");
-                        strcat(file_path,pacman);
-                        strcpy(temp_board->pacman_file, file_path); // agora basicamente falta ler os fantasmas e os pacs para o board
+
+                    for (int i = 4; buffer[i] != '\n'; i += 3){
+                        sscanf(buffer + i, " %s.p", pacman);
+
+                        build_filepath(&file_path, argv[1], pacman);
+
+                        pacman = malloc(sizeof(pacman_t));
+                        
+                        //strcpy(new_board->pacman_file, file_path); // agora basicamente falta ler os fantasmas e os pacs para o board
+                        create_creature(&pacman, file_path); // agora basicamente falta ler os fantasmas e os pacs para o board
+
+                        new_board->pacmans[n_pacmans++] = pacman;
                     }
-                    temp_board->n_pacmans = n_pacmans;
+                    new_board->n_pacmans = n_pacmans;
                 }
             }
             
 
-            //strcpy(temp_board->level_name, entry->d_name); //copia o nome do ficheiro para a estrutura do board
+            //strcpy(new_board->level_name, entry->d_name); //copia o nome do ficheiro para a estrutura do board
 
-            levels[n_levels-1]= temp_board; //adiciona o nome ao array de strings
+            levels[n_levels-1]= new_board; //adiciona o nome ao array de strings
         }
     }
 
